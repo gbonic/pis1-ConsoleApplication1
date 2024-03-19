c545642d2f16c7dc8cbd7d2581a2b5e3345856fc
gbonic <gbonic00@fesb.hr>
2024-03-19 18:58:46 +0100

    Added a second line.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -7,6 +7,8 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "New line!\n";
+    std::cout << "New second line!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
